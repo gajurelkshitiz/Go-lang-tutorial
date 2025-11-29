92440d6ad09619cce2006cc35bdedc5d28d0762c
gajurelkshitiz <kshitiz.purbct042@gmail.com>
2025-11-29 21:23:03 +0545

    if-else in go lang

---
+++ 6_if_else/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	// age := 10
+
+	// if age >= 18 {
+	// 	fmt.Println("Adult")
+	// } else {
+	// 	fmt.Println("Child")
+	// }
+
+	// if age >= 60 {
+	// 	fmt.Println("Old")
+	// } else if age >= 18 {
+	// 	fmt.Println("Adult")
+	// } else if age >= 12 {
+	// 	fmt.Println("Teen")
+	// } else {
+	// 	fmt.Println("Kid")
+	// }
+
+
+	// var role = "admin"
+	// var hasPermission = true
+
+	// if role == "admin" && hasPermission {			//   && -> and  , || -> or
+	// 	fmt.Println("Yes")
+	// }
+
+
+	// we can declare a variable inside if construct
+	// if age := 5; age >= 18 {
+	// 	fmt.Println("Person is Adult", age)
+	// } else if age >= 12 {
+	// 	fmt.Println("Person is Teen", age)
+	// } else {
+	// 	fmt.Println("Person is Child", age)
+	// }
+
+
+
+	// go doesn't have ternary operators, we'll have to use normal if-else
+}
+
