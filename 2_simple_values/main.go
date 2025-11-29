7242fb9bb6bf57bc3c1765fe72875a0aeb2ca009
gajurelkshitiz <kshitiz.purbct042@gmail.com>
2025-11-29 16:51:28 +0545

    Data types in go

---
+++ 2_simple_values/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple values
+
+	// int
+	fmt.Println(1)
+
+	// string
+	fmt.Println("hello golang")
+
+	// boolean
+	fmt.Println(false)
+	fmt.Println(1 == 1)
+
+	// float
+	fmt.Println(3.14)
+	fmt.Println(10.3/3.22)
+
+}
