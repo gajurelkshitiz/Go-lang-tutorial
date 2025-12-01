3024cc4a28e2617faec6da3a58b6cf63583f47a3
gajurelkshitiz <kshitiz.purbct042@gmail.com>
2025-12-01 10:34:36 +0545

    slices in go

---
+++ 9_slice/slice.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// slice is used for:
+// Dynamic arrays
+// most used construct in go
+
+func main() {
+	// uninitialized slices are nil
+	var nums[] int
+
+	fmt.Println(nums)
+}
